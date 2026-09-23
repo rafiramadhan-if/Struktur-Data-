bf6a4db4f42687496fa5548335ff801737604faf
rafiramadhan <rafirama1020@gmail.com>
2026-09-23 09:20:29 +0700

    Add soal3.cpp for pattern printing program

---
+++ Modul 1/soal3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    cout <<  endl;
+
+    for (int i = n; i >= 1; i--) {
+        for (int j = n; j > i; j--) {
+            cout << "  ";
+        }
+
+        for (int j = i; j >= 1; j--) {
+            cout << j << " ";
+        }
+
+        cout << "* ";
+
+        for (int j = 1; j <= i; j++) {
+            cout << j << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
